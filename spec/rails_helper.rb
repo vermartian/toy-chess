c5a298d09e1d19d423c6daeae86d252dfac60034
vermartian <vermartian@gmail.com>
2016-01-12 22:12:02 -0500

    initial commit

---
+++ spec/rails_helper.rb
@@ -0,0 +1,19 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require 'spec_helper'
+require 'rspec/rails'
+
+require File.join(File.dirname(__FILE__), 'support/valid_attribute')
+require File.join(File.dirname(__FILE__), 'support/factory_girl')
+require 'capybara/rspec'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+end
