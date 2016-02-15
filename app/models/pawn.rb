5b0d7de5a6c1e61f2ca38d31dbc92cca1c99a036
vermartian <vermartian@gmail.com>
2016-02-14 21:09:30 -0500

    little cleanup and corrected pawn attacks

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -2,14 +2,26 @@ class Pawn < Piece
 
   def vectors
     if color == false
-      vectors = [[0, 1], [1, 1], [-1, 1]]
+      vectors = [[0, 1]]
       if y == 1
         vectors << [0, 2]
       end
+      if game.piece_at?(x + 1, y + 1) && game.piece_at(x + 1, y + 1).color
+        vectors << [1, 1]
+      end
+      if game.piece_at?(x - 1, y + 1) && game.piece_at(x - 1, y + 1).color
+        vectors << [-1, 1]
+      end
     else
-      vectors = [[0, -1], [-1, -1], [1, -1]]
+      vectors = [[0, -1]]
       if y == 6
         vectors << [0, -2]
+      end
+      if game.piece_at?(x - 1, y - 1) && !game.piece_at(x - 1, y - 1).color
+        vectors << [-1, -1]
+      end
+      if game.piece_at?(x + 1, y - 1) && !game.piece_at(x + 1, y - 1).color
+        vectors << [1, -1]
       end
     end
     vectors
